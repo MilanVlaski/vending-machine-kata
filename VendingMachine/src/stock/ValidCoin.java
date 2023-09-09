4611e196ab4a7039828bf6038204ca6fc827f584
MilanVlaski <milan.vlaski321@gmail.com>
2023-09-09 13:12:05 +0200

    largestCoinWorthLessThan method now doesnt depend on enum order

--- VendingMachine/src/stock/ValidCoin.java
+++ VendingMachine/src/stock/ValidCoin.java
@@ -1,45 +1,49 @@
 package stock;
 
+import java.util.Comparator;
+import java.util.EnumSet;
+import java.util.Optional;
+
 public enum ValidCoin {
-	// valid coins must go in descending order, otherwise largestCoin method breaks
 	QUARTER(0.25),
 	DIME(0.10),
 	NICKEL(0.05);
-	
+
 	public double value;
-	
-	ValidCoin (double value) {
+
+	ValidCoin(double value) {
 		this.value = value;
 	}
-// If the amount can't be properly represented with coins, we can't deal with it
+
 	public static ValidCoin largestCoinWorthLessThan(double amount) {
-		for (ValidCoin coin : ValidCoin.values()) {
-			if(coin.value <= amount) {
-				return coin;
-			}
-		}
-		return null;
+		Optional<ValidCoin> largestCoin = EnumSet.allOf(ValidCoin.class)
+					.stream()
+					.filter(coin -> coin.value <= amount)
+					.max(Comparator.comparingDouble(coin -> coin.value));
+		return largestCoin.orElse(null);
 	}
+
 	/**
-	 * If you put in gibberish, or a coin that is not allowed,
-	 * this function returns 0. Otherwise, returns the actual value as specified
-	 * in the ValidCoin enum.
+	 * If you put in gibberish, or a coin that is not allowed, this function returns
+	 * 0. Otherwise, returns the actual value as specified in the ValidCoin enum.
+	 * 
 	 * @param string representation of coin
 	 * @return
 	 */
 	public static double valueOfCoin(String typeOfCoin) {
-		
+
 		double result = 0;
 		for (ValidCoin c : ValidCoin.values()) {
-			if(typeOfCoin.equals(c.toString()))
+			if (typeOfCoin.equals(c.toString()))
 				result = c.value;
 		}
-		
+
 		return result;
 	}
-	
+
 	@Override
 	public String toString() {
 		return name().toLowerCase();
 	}
+
 }
