2c62e4df12a8ac020eab5fb30293c827420be544
MilanVlaski <milan.vlaski321@gmail.com>
2023-09-09 15:01:06 +0200

    wrote min and max functions for ValidCoin

--- VendingMachine/src/stock/ValidCoin.java
+++ VendingMachine/src/stock/ValidCoin.java
@@ -2,7 +2,8 @@ package stock;
 
 import java.util.Comparator;
 import java.util.EnumSet;
-import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Stream;
 
 public enum ValidCoin {
 	QUARTER(0.25),
@@ -15,10 +16,11 @@ public enum ValidCoin {
 		this.value = value;
 	}
 
+	private static Stream<ValidCoin> stream = EnumSet.allOf(ValidCoin.class).stream();
+	
 	public static ValidCoin largestCoinWorthLessThan(double amount) {
 
-		return EnumSet.allOf(ValidCoin.class)
-				.stream()
+		return stream
 				.filter(coin -> coin.value <= amount)
 				.max(Comparator.comparingDouble(coin -> coin.value))
 				.orElse(null);
@@ -33,17 +35,25 @@ public enum ValidCoin {
 	 */
 	public static Double valueOfCoin(String typeOfCoin) {
 
-		return EnumSet.allOf(ValidCoin.class)
-				.stream()
-				.filter(c -> typeOfCoin.equals(c.toString()))
+		return stream.filter(c -> typeOfCoin.equals(c.toString()))
 				.map(c -> c.value)
 				.findFirst()
 				.orElse(0.0);
 	}
-
+	
+	public static ValidCoin max() {
+		return stream
+				.max((c1, c2) -> Double.compare(c1.value, c2.value))
+				.orElseThrow();
+	}
+	
+	public static ValidCoin min() {
+		return stream.min((c1, c2) -> Double.compare(c1.value, c2.value))
+				.orElseThrow();
+	}
+	
 	@Override
 	public String toString() {
 		return name().toLowerCase();
 	}
-
 }
